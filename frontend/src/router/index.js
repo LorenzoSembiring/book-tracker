d0892cf5ca830f3cb86a656f8a708bc94b1ec916
Lorenzo Laridho Sembiring <68765098+LorenzoSembiring@users.noreply.github.com>
2024-08-20 18:06:19 +0700

    feat(fe): register view layout

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,8 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '@/views/Home.vue'
+import Register from '@/views/Register.vue'
 
 const routes = [
   { path: '/', component: Home },
+  { path: '/register', component: Register },
 ]
 
 const router = createRouter({
---
+++ frontend/src/views/Register.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="container py-20 bg-cyan-50 h-screen sm:px-0 sm:max-w-full">
+    <div class="">
+      <div class="flex content-center justify-center">
+        <div class="border rounded bg-white items-center px-4 sm:px-16 sm:py-3">
+          <div class="flex justify-center flex-col items-center">
+            <div class="font-bold text-cyan-900 text-2xl pt-5">Sign Up</div>
+            <div class="text-black">
+              <div>
+                or
+                <div class="font-semibold text-cyan-800 inline">
+                  <a href="/login">sign in to your account</a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="m-4">
+            <div class="p-3 border rounded-md m-3">
+              <div><input type="text" size="20" placeholder="Username"/></div>
+            </div>
+            <div class="p-3 border rounded-md m-3">
+              <div><input type="text" size="20" placeholder="Email"/></div>
+            </div>
+            <div class="p-3 border rounded-md m-3">
+              <div><input type="password" size="17" placeholder="Password"/></div>
+            </div>
+            <div class="p-3 border rounded-md m-3">
+              <div><input type="password" size="17" placeholder="Confirm Password"/></div>
+            </div>
+          </div>
+          <div class="flex justify-center pb-5">
+            <button class="border rounded-md px-4 py-2 bg-cyan-500 text-white font-bold">
+              Sign Up
+            </button>
+          </div>
+          <div class="flex flex-col items-center pb-5">
+            <div class="text-gray-500 font-medium">
+              Or continue with
+            </div>
+            <button class="bg-gray-200 rounded p-3 inline-block">
+              <Icon class="text-gray-600" icon="ri:google-fill"/>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script setup>
+import { Icon } from '@iconify/vue';
+
+</script>
+<style scoped>
+.libre-baskerville-bold {
+  font-family: "Libre Baskerville", serif;
+  font-weight: 900;
+  font-style: normal;
+}
+textarea:focus,
+input:focus {
+  outline: none;
+}
+</style>
