038d1b34b92d46d8e7b3edd57fea3b577b863466
Lorenzo Laridho Sembiring <68765098+LorenzoSembiring@users.noreply.github.com>
2024-08-21 10:18:36 +0700

    feat(fe): not found view

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,8 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '@/views/Home.vue'
 import Register from '@/views/Register.vue'
+import NotFound from '@/views/NotFound.vue'
 
 const routes = [
+  { path: '/:pathMatch(.*)*', name: 'NotFound', component: NotFound },
   { path: '/', component: Home },
   { path: '/register', component: Register },
 ]
---
+++ frontend/src/views/NotFound.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="h-screen flex text-center place-items-center">
+    <div class="">
+      <div class="font-bold text-4xl text-cyan-500">404</div>
+      <div class="pt-2 text-lg font-semibold text-cyan-800">
+        The page you're looking for doesn't exist.
+      </div>
+      <div class="pt-4">
+        <button
+				@click="goToHomepage"
+          class="border rounded-md px-4 py-2 bg-cyan-500 text-white font-bold"
+        >
+          Back to Homepage
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+<script setup>
+import router from "../router/index.js"
+
+function goToHomepage() {
+  router.push("/")
+}
+</script>
+<style scoped>
+.plus-jakarta-sans {
+  font-family: "Plus Jakarta Sans", sans-serif;
+  font-optical-sizing: auto;
+  font-weight: 300;
+  font-style: normal;
+}
+.plus-jakarta-sans-bold {
+  font-family: "Plus Jakarta Sans", sans-serif;
+  font-optical-sizing: auto;
+  font-weight: 500;
+  font-style: normal;
+}
+</style>
