4931900504a8102b9ec2e659204560f5b9bf1742
Lorenzo Laridho Sembiring <68765098+LorenzoSembiring@users.noreply.github.com>
2024-08-30 23:30:40 +0700

    feat(be): user model select

--- backend/src/model/User.js
+++ backend/src/model/User.js
@@ -8,6 +8,22 @@ class User{
     static async insert(email, username, password){
         return await db('users').insert({email: email, username: username, password: password});
     }
+    static async select({ email, username, id }) {
+        const query = db('users');
+    
+        if (email) {
+            query.where({ email });
+        } else if (username) {
+            query.where({ username });
+        } else if (id) {
+            query.where({ id });
+        } else {
+            return null;
+        }
+    
+        return await query.select();
+    }
+
 }
 
 module.exports = User;
