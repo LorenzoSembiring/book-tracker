913b1354bd6a4f6da938785fb28c24a406cc01b6
Lorenzo Laridho Sembiring <68765098+LorenzoSembiring@users.noreply.github.com>
2024-09-04 19:51:13 +0700

    fix(be): fix register return response

--- backend/src/controller/authController.js
+++ backend/src/controller/authController.js
@@ -10,27 +10,28 @@ class authController{
 
         try {
             const register = await authService.register(username, email, password, "user");
-            
-            if (typeof register == String) {
-                res.status(409).json({
+            console.log(typeof register)
+            if (typeof register == "string") {
+                return res.status(409).json({
                     code: 409,
                     status: "conflict",
                     message: register
                 })
             }
-            res.status(201).json({
+            return res.status(201).json({
                 code: 201,
                 status: "created",
                 data: register
             })
         } catch (error) {
-            res.status(500).json({
+            return res.status(500).json({
                 code: 500,
                 status: "error",
                 message: error
             })
             
         }
+    }
 
     }
 }
