807674d5a8b4e50aec62353e621aa2795d9663ed
Lorenzo Laridho Sembiring <68765098+LorenzoSembiring@users.noreply.github.com>
2024-09-17 21:43:32 +0700

    feat(be): user repository get all, select, insert

---
+++ backend/src/repository/userRepository.js
@@ -0,0 +1,29 @@
+const db = require('../../config/database')
+class User{
+    static tableName = 'users';
+
+    static async getAll(){
+        return db.select().table('users');
+    }
+    static async insert(email, username, password, role){
+        return await db('users').insert({email: email, username: username, password: password, role: role});
+    }
+    static async select({ email, username, id }) {
+        const query = db('users');
+    
+        if (email) {
+            query.where({ email });
+        } else if (username) {
+            query.where({ username });
+        } else if (id) {
+            query.where({ id });
+        } else {
+            return null;
+        }
+    
+        return await query.select();
+    }
+
+}
+
+module.exports = User;
