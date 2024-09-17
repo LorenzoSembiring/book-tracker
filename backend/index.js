b5b52028692315eb5b358fea8f0063c55545eb7b
Lorenzo Laridho Sembiring <68765098+LorenzoSembiring@users.noreply.github.com>
2024-09-17 17:38:41 +0700

    feat(be): add cors

--- backend/index.js
+++ backend/index.js
@@ -2,12 +2,14 @@ const express = require('express');
 const userRoute = require('./src/route/userRoute');
 const route = require('./src/route/index');
 require('dotenv').config();
+var cors = require('cors')
 const app = express();
 const port = process.env.PORT
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+app.use(cors())
 app.use('/api/', route)
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
