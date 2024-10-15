97cc367531c32013dd095c26c9e1c73161b86a45
Lorenzo Laridho Sembiring <68765098+LorenzoSembiring@users.noreply.github.com>
2024-10-15 09:54:12 +0700

    feat(fe): dashboard view search

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -3,12 +3,14 @@ import Home from '@/views/Home.vue'
 import Register from '@/views/Register.vue'
 import Login from '@/views/Login.vue'
 import NotFound from '@/views/NotFound.vue'
+import Dashboard from '@/views/Dashboard.vue'
 
 const routes = [
   { path: '/:pathMatch(.*)*', name: 'NotFound', component: NotFound },
   { path: '/', component: Home },
   { path: '/register', component: Register },
   { path: '/login', component: Login },
+  { path: '/dashboard', component: Dashboard },
 ]
 
 const router = createRouter({
---
+++ frontend/src/views/Dashboard.vue
@@ -0,0 +1,45 @@
+<template>
+    <Navbar />
+    <div class="grid justify-items-center">
+        <div class="md:text-5xl text-4xl plus-jakarta-sans text-gray-600 mt-40 text-center">Add your book</div>
+        <div class="text-lg plus-jakarta-sans text-gray-400 mt-3 mb-10">Search or input your current reading</div>
+        <div class="w-56 sm:w-80">
+            <label class="input input-bordered flex items-center gap-2">
+                <input
+                    type="text"
+                    class="grow"
+                    placeholder="Search"
+                    v-model="searchInput"
+                />
+                <Icon
+                    @click="search()"
+                    class="grow text-2xl cursor-pointer"
+                    icon="ic:round-search"
+                />
+            </label>
+        </div>
+        <div class="text-md plus-jakarta-sans text-gray-600 mt-1 mb-10">Not finding your book? <span class="cursor-pointer text-blue-500">Insert it manually</span></div>
+    </div>
+</template>
+
+<script setup>
+import Navbar from "@/components/navbar.vue";
+import { Icon } from "@iconify/vue";
+
+const searchInput = defineModel("searchInput");
+
+function search() {}
+</script>
+<style scoped>
+.input:focus,
+.input:focus-within {
+    outline-style: none;
+}
+
+.plus-jakarta-sans {
+    font-family: "Plus Jakarta Sans", sans-serif;
+    font-optical-sizing: auto;
+    font-weight: 500;
+    font-style: normal;
+}
+</style>
